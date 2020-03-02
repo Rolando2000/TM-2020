9c81d4cac03590fd83f408d301f556f2607a3ad2
Rolando2000 <61158161+Rolando2000@users.noreply.github.com>
2020-03-02 18:04:00 +0000

    RolandoSilva-22355

---
+++ Aula 2/exercicio2/RolandoSilva-22355/script.js
@@ -0,0 +1,18 @@
+var numbers = [];
+
+function addNumber(){
+    var value = document.getElementById("numbers").value;
+    numbers.push(parseInt(value));
+    document.getElementById("numberAdded").innerText=numbers.toString();
+
+    console.log(numbers);
+}
+
+function calculate(){
+    if(numbers.length >= 5) {
+        var biggestNumber = Math.max.apply(Math,numbers);
+        document.getElementById("result").innerText=biggestNumber;
+
+    }
+
+}
